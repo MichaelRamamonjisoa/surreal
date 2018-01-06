1e325eceb46e1b284e6b2e749cb4dedaeac64dbc
Michael Ramamonjisoa <ramamonjisoa.michael@gmail.com>
2018-01-06 23:34:31 +0100

    Fixed run_batch.py Updated job_done.txt, updated job_list.txt using generate_job_list_noprefix.py

--- datageneration/run_batch.py
+++ datageneration/run_batch.py
@@ -7,12 +7,17 @@ with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datagenerati
 
 sdir = join(os.path.expanduser('~'),'Documents','MVA','datageneration')
 
-for i,job_params in enumerate(list_job):
-    if (i<20):
+with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datageneration','misc','job_done.txt'),'r') as f:
+    job_done = [line.split('\n')[0] for line in f]
+    
+nb_done = len(job_done)
+
+for i,job_params in enumerate(list_job):    
+    if i>nb_done-1 and i<nb_done + 20:
 	print('current job: %s' %job_params)
         cmd = subprocess.call(sdir +'/' +'run.sh' +' '+ '\''+job_params+'\'', shell=True)        
         with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datageneration','misc','job_done.txt'),'a') as f:
-            f.write(job_params)
+            f.write(job_params+'\n')
             
 
             
