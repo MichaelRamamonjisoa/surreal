073da4a62e8a0e299ca0f8b63901e54cdfd95ede
Rémi Lespinet <remi.lespinet@gmail.com>
2018-01-07 15:44:03 +0100

    Fix Pathes

--- datageneration/run_batch.py
+++ datageneration/run_batch.py
@@ -2,12 +2,12 @@ import os
 from os.path import join
 import subprocess
 
-with open(join(os.path.getcwd(), 'misc', 'job_list.txt'),'r') as f:
+with open(join(os.getcwd(), 'misc', 'job_list.txt'),'r') as f:
     list_job = [line.split('\n')[0] for line in f]
 
-sdir = os.path.getcwd()
+sdir = os.getcwd()
 
-with open(join(os.path.getcwd(),'misc','job_done.txt'),'r') as f:
+with open(join(os.getcwd(),'misc','job_done.txt'),'r') as f:
     job_done = [line.split('\n')[0] for line in f]
 
 nb_done = len(job_done)
@@ -16,5 +16,5 @@ for i,job_params in enumerate(list_job):
     if i>nb_done-1 and i<nb_done + 109:
         print('current job: %s' %job_params)
         cmd = subprocess.call(sdir +'/' +'run.sh' +' '+ '\''+job_params+'\'', shell=True)
-        with open(join(os.path.getcwd(),'misc','job_done.txt'),'a') as f:
+        with open(join(os.getcwd(),'misc','job_done.txt'),'a') as f:
             f.write(job_params+'\n')
