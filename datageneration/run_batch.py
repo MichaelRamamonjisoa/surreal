7c3c1f86a2174ec07dd112e916a79308e48b729d
Rémi Lespinet <remi.lespinet@gmail.com>
2018-01-07 15:07:54 +0100

    Remove hard coded path

--- datageneration/run_batch.py
+++ datageneration/run_batch.py
@@ -2,12 +2,12 @@ import os
 from os.path import join
 import subprocess
 
-with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datageneration','misc','job_list.txt'),'r') as f:
+with open(join(os.path.getcwd(), 'misc', 'job_list.txt'),'r') as f:
     list_job = [line.split('\n')[0] for line in f]
 
-sdir = join(os.path.expanduser('~'),'Documents','MVA','datageneration')
+sdir = os.path.getcwd()
 
-with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datageneration','misc','job_done.txt'),'r') as f:
+with open(join(os.path.getcwd(),'misc','job_done.txt'),'r') as f:
     job_done = [line.split('\n')[0] for line in f]
     
 nb_done = len(job_done)
@@ -15,8 +15,8 @@ nb_done = len(job_done)
 for i,job_params in enumerate(list_job):    
     if i>nb_done-1 and i<nb_done + 109:
 	print('current job: %s' %job_params)
-        cmd = subprocess.call(sdir +'/' +'run.sh' +' '+ '\''+job_params+'\'', shell=True)        
-        with open(join(os.path.expanduser('~'),'Documents','MVA','surreal','datageneration','misc','job_done.txt'),'a') as f:
+        cmd = subprocess.call(sdir +'/' +'run.sh' +' '+ '\''+job_params+'\'', shell=True)
+        with open(join(os.path.getcwd(),'misc','job_done.txt'),'a') as f:
             f.write(job_params+'\n')
             
 
